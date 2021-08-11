ec3b495a699daa94ff985543d9761b88672cce13
cominixo <22600756+cominixo@users.noreply.github.com>
2021-08-11 19:42:30 -0300

    bad translations and some fixes

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
 const { Plugin } = require('powercord/entities');
 
-const { React, getModule, getModuleByDisplayName, i18n: { Messages } } = require('powercord/webpack');
+const { React, getModule, getModuleByDisplayName, i18n } = require('powercord/webpack');
 const { inject, uninject } = require('powercord/injector');
 
-const i18n = require("./i18n");
+const lang = require("./i18n");
 const timestamps = require("./i18n/timestamps.json")
 
 module.exports = class TokiPona extends Plugin {
+  constructor () {
+    super();
+
+  }
   async startPlugin () {
-      
-    powercord.api.i18n.loadAllStrings(i18n);
+
+    powercord.api.i18n.loadAllStrings(lang);
+    // dumb hack, only a problem with the search text
+    i18n.Messages.SEARCH = lang["en-US"].SEARCH;
+
     const timestampModule = await getModule(m => m.default?.displayName === "MessageTimestamp");
     inject(
         "translate-timestamp",
@@ -18,9 +25,23 @@ module.exports = class TokiPona extends Plugin {
         (args, res) => {
             args[0].timestamp._locale = Object.assign(args[0].timestamp._locale, timestamps);
             return res;
-        
+
         }
     );
+
+    const test = await getModule(m => m.default?.displayName === "SearchBar");
+    inject(
+        "search-test",
+        test,
+        "default",
+        (args, res) => {
+            console.log("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa\n");
+            console.log(res);
+            return res;
+
+        }
+    );
+
   }
 
   pluginWillUnload () {
