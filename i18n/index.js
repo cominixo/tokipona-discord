6d360d0d94e931d1f3e7d1b9e038a2ac86f609a4
Kylie McClain <kylie@somas.is>
2021-12-13 12:42:00 -0500

    .prettierrc: initial; format js files

--- i18n/index.js
+++ i18n/index.js
@@ -1,9 +1,9 @@
-require('fs')
-  .readdirSync(__dirname)
-  .filter(file => file !== 'index.js')
-  .forEach(filename => {
-    if (filename != "timestamps.json") {
-        exports['en-GB'] = require(`${__dirname}/${filename}`);
-        exports['en-US'] = require(`${__dirname}/${filename}`);
-    }
-  });
+require("fs")
+    .readdirSync(__dirname)
+    .filter((file) => file !== "index.js")
+    .forEach((filename) => {
+        if (filename != "timestamps.json") {
+            exports["en-GB"] = require(`${__dirname}/${filename}`);
+            exports["en-US"] = require(`${__dirname}/${filename}`);
+        }
+    });
--- index.js
+++ index.js
@@ -1,42 +1,47 @@
-const { Plugin } = require('powercord/entities');
+const { Plugin } = require("powercord/entities");
 
-const { React, getModule, getModuleByDisplayName, i18n } = require('powercord/webpack');
-const { inject, uninject } = require('powercord/injector');
+const {
+    React,
+    getModule,
+    getModuleByDisplayName,
+    i18n,
+} = require("powercord/webpack");
+const { inject, uninject } = require("powercord/injector");
 
 const lang = require("./i18n");
-const timestamps = require("./i18n/timestamps.json")
+const timestamps = require("./i18n/timestamps.json");
 
 module.exports = class TokiPona extends Plugin {
-  constructor () {
-    super();
-
-  }
-  async startPlugin () {
-
-    powercord.api.i18n.loadAllStrings(lang);
-    // dumb hack for things that don't load properly with loadAllStrings
-    for (const [key, value] of Object.entries(i18n.Messages)) {
-        if (typeof i18n.Messages[key] == "string") {
-            i18n.Messages[key] = lang["en-US"][key];
-
+    constructor() {
+        super();
+    }
+    async startPlugin() {
+        powercord.api.i18n.loadAllStrings(lang);
+        // dumb hack for things that don't load properly with loadAllStrings
+        for (const [key, value] of Object.entries(i18n.Messages)) {
+            if (typeof i18n.Messages[key] == "string") {
+                i18n.Messages[key] = lang["en-US"][key];
+            }
         }
+
+        const timestampModule = await getModule(
+            (m) => m.default?.displayName === "MessageTimestamp"
+        );
+        inject(
+            "translate-timestamp",
+            timestampModule,
+            "default",
+            (args, res) => {
+                args[0].timestamp._locale = Object.assign(
+                    args[0].timestamp._locale,
+                    timestamps
+                );
+                return res;
+            }
+        );
     }
 
-    const timestampModule = await getModule(m => m.default?.displayName === "MessageTimestamp");
-    inject(
-        "translate-timestamp",
-        timestampModule,
-        "default",
-        (args, res) => {
-            args[0].timestamp._locale = Object.assign(args[0].timestamp._locale, timestamps);
-            return res;
-
-        }
-    );
-
-  }
-
-  pluginWillUnload () {
-    uninject("translate-timestamp");
-  }
+    pluginWillUnload() {
+        uninject("translate-timestamp");
+    }
 };
