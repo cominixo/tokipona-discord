5302ad103dc6e1cacb77e5aac840573a19f1ed70
Kylie McClain <kylie@somas.is>
2022-06-20 01:58:30 -0400

    *.sh: o pana sona pi nasin ilo

--- mama_pi_ante_toki.sh
+++ mama_pi_ante_toki.sh
@@ -1,11 +1,25 @@
 #!/bin/sh
 
+# lipu ilo ni li pali e ni:
+#   1. o jo e nimi lon toki mama. o jo e nimi lon toki ante.
+#   2. kepeken ilo grep(1) la, o lukin e lipu nimi tu ni.
+#      o weka e nimi lon ala lipu tu taso.
+#   3. o open e lipu pi nimi ni taso.
+#   4. kepeken ilo jq(1) la, o pona e nasin pi lipu sin.
+#      (wile sona pi nasin pona la, o lukin e toki mi
+#      lon lipu ilo toki_mama.sh)
+
 set -e
 [ "$#" -gt 0 ] || set -- mama_pi_ante_toki.json
 
 nimi_taso=$(mktemp)
 taso=$(mktemp)
 pona=$(mktemp)
+
+# nimi $PS3 li weka tan ni: toki tan ilo make(1) la, ni li kama sama lukin
+PS3=
+
+set -x
 
 grep -f nimi_taso_lon_toki_ale.txt toki_mama_ale.json >"${nimi_taso}"
 
@@ -18,5 +32,6 @@ sed \
     >"${taso}"
 
 jq --indent 4 -S <"${taso}" >"${pona}"
+
 mv "${pona}" "$1"
 rm -f "${nimi_taso}" "${taso}"
--- toki_mama.sh
+++ toki_mama.sh
@@ -1,5 +1,16 @@
 #!/bin/sh
 
+# lipu ilo ni li pali e ni:
+#   1. kepeken ilo curl(1) la, o kama jo e lipu sin pi ilo Siko
+#   2. kepeken ilo sed(1) kepeken ilo grep(1) la, o nasin e toki tan lipu.
+#      o weka e toki ala tan lipu, o jo e toki taso. o jo ala e toki ilo.
+#   3. kepeken ilo jq(1) la, o pona e nasin pi toki mama.
+#      nasin pona li ni:
+#       open lipu la, nimi lili A, en nimi lili E, en nimi lili I li lon.
+#       pini lipu la, nimi lili T, en nimi lili U, en nimi lili W li lon.
+
+set -e
+
 url="https://raw.githubusercontent.com/Discord-Datamining/Discord-Datamining/master/current.js"
 [ "$#" -gt 0 ] || set -- toki_mama_ale.json
 
@@ -7,9 +18,12 @@ mama=$(mktemp)
 taso=$(mktemp)
 pona=$(mktemp)
 
-set -e
+# nimi $PS3 li weka tan ni: toki tan ilo make(1) la, ni li kama sama lukin
+PS3=
 
-curl -Lfs -o "${mama}" "${url}"
+set -x
+
+curl -Lf# -o "${mama}" "${url}"
 
 sed "s/^\s*//" "${mama}" \
     | grep -E \
